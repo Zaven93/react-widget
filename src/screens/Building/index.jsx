1aa282933f87f55e034b3f85e82130811bfd8d9b
Zaven <zaven.avanesyan93@gmail.com>
2021-11-25 16:00:50 +0400

    Commit

--- src/screens/Building/index.jsx
+++ src/screens/Building/index.jsx
@@ -45,7 +45,7 @@ const BuildingInner = ({ nextLink, prevLink, hash }) => {
       <div className="lg:flex lg:flex-row-reverse">
         <div className="lg:w-8/12 xl:w-9/12 lg:pl-2">
           <img
-            src="https://via.placeholder.com/1436x1080"
+            src="https://res.cloudinary.com/dpwbaabx2/image/upload/v1637840875/qicuhleb3p3favujg1ov.png"
             alt="description"
             loading="lazy"
             width="1436"
@@ -61,7 +61,7 @@ const BuildingInner = ({ nextLink, prevLink, hash }) => {
           <div className="building-widget-block mx-auto">
             <div className="building-widget__logo text-center pb-5 mx-auto mb-16">
               <img
-                src="https://via.placeholder.com/193x86"
+                src="https://res.cloudinary.com/dpwbaabx2/image/upload/v1637841004/euz7n8h5jsg3cd8nh8jc.png"
                 alt="description"
                 loading="lazy"
                 width="193"
